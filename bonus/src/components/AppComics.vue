39bc37669b78bc8408079a9917e18f4db73948e1
SimoneVilardo <simovila95@gmail.com>
2023-06-21 16:22:29 +0200

    ampliato esercizio  del 21/06 nella cartella bonus

---
+++ bonus/src/components/AppComics.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    props:{
+        myComic: Object
+    }
+}
+</script>
+<template >
+    <div class="fumetto">
+        <img :src="myComic.thumb">
+        <h4>{{ myComic.series }}</h4>
+        <p><strong>Type: {{ myComic.type }}</strong></p>
+    </div>
+</template>
+<style lang="scss" scoped>
+    img{
+        width: 200px;
+        height: 200px;
+        padding: 20px;
+    }
+
+    h4{
+        padding: 0 20px;
+    }
+
+    p{
+        padding: 0 20px;
+        font-size: 10px;
+    }
+</style>
